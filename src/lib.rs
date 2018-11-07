08a6b60ddadc5f652bbb5558c5295b4fe39b56ed
cuixianming <cuixianming@gotokeep.com>
2018-11-07 19:57:14 +0800

    rewrite lib

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,17 @@ pub fn say(what: &str) -> String {
 
 pub fn get_all_file(dir_path: &str) -> HashMap<PathBuf, Vec<PathBuf>> {
     let mut map = HashMap::new();
-    for d in fs::read_dir(dir_path).unwrap() {
-        let dir = d.unwrap();
-        let dir_name = dir.path();
-        let files = fs::read_dir(dir.path()).unwrap()
-            .map(|e| e.unwrap().path())
-            .collect::<Vec<PathBuf>>();
-        map.insert(dir_name, files);
+    if let Ok(mut dirs) = fs::read_dir(dir_path) {
+        while let Some(Ok(dir)) = dirs.next() {
+            let dir_name = dir.path();
+            if let Ok(files) = fs::read_dir(&dir_name) {
+                let file_list = files
+                    .filter(|e| e.is_ok())
+                    .map(|e| e.unwrap().path())
+                    .collect();
+                map.insert(dir_name, file_list);
+            }
+        }
     }
     map
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 extern crate spam_classifier;
 
+use std::env;
+
 fn main() {
-    let files = spam_classifier::get_all_file("/Users/xana/Dev/data/20_newsgroups/");
-    println!("{:?}", files);
+    let mut args = env::args();
+    args.next();
+    let dir_path = match args.next() {
+        Some(path) => path,
+        None => env::current_dir().unwrap().join("data/20_newsgroups/").to_str().unwrap().to_owned()
+    };
+    let files = spam_classifier::get_all_file(&dir_path);
+    for (key, _list) in files {
+        println!("{:?}", key);
+    }
 }
